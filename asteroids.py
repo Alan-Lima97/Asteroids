4c9e39acf42fd911342ef2f4c4cede9d307eb1e3
Alan-Lima97 <alanldsk@gmail.com>
2025-08-26 16:22:40 -0300

    Define Asteroid class with position, movement, and collision detection

---
+++ asteroids.py
@@ -0,0 +1,30 @@
+import pygame
+import random
+from circleshape import CircleShape
+from constants import *
+
+class Asteroid(CircleShape):
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+                
+    def draw(self, screen):
+        center = (int(self.position.x), int(self.position.y))
+        pygame.draw.circle(screen, (255,255,255), center, int(self.radius), 2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt 
+
+    def split (self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        angle = random.uniform(20, 50)
+        v1 = self.velocity.rotate(angle)
+        v2 = self.velocity.rotate(-angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        new_asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid1.velocity = v1 * 1.2
+        new_asteroid2.velocity = v2 * 1.2
+            
